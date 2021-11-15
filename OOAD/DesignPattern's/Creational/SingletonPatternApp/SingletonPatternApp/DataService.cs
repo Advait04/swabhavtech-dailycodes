bfe09a96beae82714535e2c6e3beb801ae363b80
advait <kunteadvait04@gmail.com>
2021-11-15 19:18:21 +0530

    SingletonPatternApp

---
+++ OOAD/DesignPattern's/Creational/SingletonPatternApp/SingletonPatternApp/DataService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPatternApp
+{
+   public class DataService
+    {
+       private static DataService bucket;
+        private  DataService()
+        {
+            
+            Console.WriteLine("DataService Created");
+        }
+
+        public void DoSomething()
+        {
+            Console.WriteLine("Work Done By" + this.GetHashCode());
+        }
+
+       public static DataService GetInstance()
+        {
+            if (bucket == null)
+                bucket = new DataService();
+            return bucket;
+        }
+
+    }
+}
---
+++ OOAD/DesignPattern's/Creational/SingletonPatternApp/SingletonPatternApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SingletonPatternApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(() =>
+
+            {
+                DataService.GetInstance().DoSomething();
+            });
+
+            Thread t2 = new Thread(() =>
+            {
+                DataService.GetInstance().DoSomething();
+            });
+            t1.Start();
+            t2.Start();
+
+            Console.ReadKey();
+
+
+        }
+        public static void CaseStudy1()
+        {
+            var service1 = DataService.GetInstance();
+            var service2 = DataService.GetInstance();
+            Console.WriteLine(service1.GetHashCode());
+            Console.WriteLine(service2.GetHashCode());
+
+            service1.DoSomething();
+            service2.DoSomething();
+            Console.ReadKey();
+        }
+    }
+}
+

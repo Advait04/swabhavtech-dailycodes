71790afa2804a06d1d4256e26cf66cf058622fac
advait <kunteadvait04@gmail.com>
2021-11-15 16:53:20 +0530

    FacadeDesignPatternSolution

---
+++ SolidPrinciple/FacadeDesignPatternSolution/FacadeDesignPatternApp/PizzaProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeDesignPatternApp
+{
+   public class PizzaProvider:IPizza
+    {
+        public void GetNonVegPizza()
+        {
+            GetNonVegToppings();
+            Console.WriteLine("Get Non-Veg Pizza...");
+        }
+        public void GetVegPizza()
+        {
+
+            Console.WriteLine("Get Non-Veg Pizza...");
+        }
+        public void GetNonVegToppings()
+        {
+            Console.WriteLine("Getting Non-Veg Pizza Toppings...");
+        }
+    }
+}
---
+++ SolidPrinciple/FacadeDesignPatternSolution/FacadeDesignPatternApp/RestaurantFacade.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FacadeDesignPatternApp
+{
+    class RestaurantFacade
+    {
+        private IPizza _pizzaProvider;
+        private IBread _breadProvider;
+        public RestaurantFacade()
+        {
+            _pizzaProvider = new PizzaProvider();
+            _breadProvider = new BreadProvider();
+        }
+        public void GetNonVegPizza()
+        {
+            _pizzaProvider.GetNonVegPizza();
+        }
+        public void GetVegPizza()
+        {
+            _pizzaProvider.GetVegPizza();
+        }
+        public void GetGarlicBread()
+        {
+            _breadProvider.GetGarlicBread();
+        }
+        public void GetCheesyGarlicBread()
+        {
+            _breadProvider.GetCheesyGarlicBread();
+        }
+    }
+}

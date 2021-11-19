c6ddcb2e8b65badfdcc18683e742cfd8de71ccbf
advait <kunteadvait04@gmail.com>
2021-11-19 14:04:25 +0530

    DOMGenerator Gnerator

---
+++ OOAD/DOMGeneratorApp/DOMGeneratorApp/Model/ITag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOMGeneratorApp.Model
+{
+    public interface ITag
+    {
+        string Render(string htmlString);
+    }
+}
---
+++ OOAD/DOMGeneratorApp/DOMGeneratorApp/Model/IndividualTag.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DOMGeneratorApp.Model
+{
+    public class IndividualTag:ITag
+    {
+        private string _name;
+        private string _type;
+    
+        public IndividualTag(string name, string type)
+        {
+            _type = type;
+            _name = name;
+        }
+        public IndividualTag(string name) : this(name, " ") { }
+
+        public string Render(string htmlString)
+        {
+              if (_name == "input") return htmlString + "<" + _name + " type=\"" + _type + "\">";
+              return htmlString + "<" + _name + "/>";
+        }
+    }
+}

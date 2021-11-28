a742d429e27a61f7c6c27be7ed06ea6c89438071
advait <kunteadvait04@gmail.com>
2021-11-28 14:07:53 +0530

    calsswork

---
+++ OOP/SimpleDelegateApp/SimpleDelegateApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDelegateApp
+{
+    delegate void DPrintMessage(string name);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            DPrintMessage f1;
+            f1 = PrintHello;
+            f1("Advait");
+
+            f1 = PrintGoodbye;
+            f1("Rohit");
+           
+            MessageWizard(f1);
+            MessageWizard(PrintHello);
+            MessageWizard(PrintGoodbye);
+
+            DPrintMessage f2;
+            f2 = PrintHello;
+            f2 += PrintGoodbye;
+            f2 += PrintHello;
+            f2 += PrintGoodbye;
+
+            f2("Swabhav Tech");
+
+            Console.ReadKey();
+        }
+        public static void PrintHello(string name)
+        {
+            Console.WriteLine("Hello Says {0}", name);
+        }
+        public static void PrintGoodbye(string name)
+        {
+            Console.WriteLine("GoodBye says {0}", name);
+        }
+        public static void PrintFoo()
+        {
+
+        }
+        public static void MessageWizard(DPrintMessage funct)
+        {
+            Console.WriteLine("Inside MessageWizard");
+            funct("CHAMP");
+        }
+    }
+}
